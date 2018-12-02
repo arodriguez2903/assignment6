135664fca24fbb53d812210a629ee454b5f097ce
Andrea Rodriguez <arodriguez29@ufl.edu>
2018-12-02 12:36:16 -0500

    Edited javascript

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+//array
+var members = ["Dave Grohl", "Nate Mendel", "Pat Smear", "Taylor Hawkins", "Rami Jaffee", "Chris Shiflett"];
+
+//object
+var tour = {Chicago: "July 29th & 30th, 2018",
+        Seattle: "September 1st, 2018",
+        Edmonton: "September 4th, 2018"};
+
+var i = 1;
+while (i <= 5) {
+  console.log("Times running through the loop "+ i);
+  i++;
+};
